140cdd5e3f5ede20015db3a44f3cb1d3bcc7e33d
ycoplusone <ycoplusone@naver.com>
2024-04-24 17:26:31 +0900

    20240424

--- kaggle/guide/t9998.py
+++ kaggle/guide/t9998.py
@@ -1,14 +1,5 @@
 import pandas as pd
 
-# MultiIndex를 가진 DataFrame 생성
-arrays = [['A', 'A', 'B', 'B'], [1, 2, 1, 2]]
-index = pd.MultiIndex.from_arrays(arrays, names=('first', 'second'))
-df = pd.DataFrame({'data': [1, 2, 3, 4]}, index=index)
 
-# MultiIndex를 일반적인 DataFrame으로 변경
-df_reset = df.reset_index()
-
-#print("Original DataFrame:")
-print(df.info())
-#print("\nDataFrame after reset_index():")
-print(df_reset.info())
+str = "abc{0} dddd{1}"
+print( str.format('ddddddd','2121212') )

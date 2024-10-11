4bdcaa62704b1104d0450c2da1e00bc17f3f5832
ycoplusone <ycoplusone@naver.com>
2024-10-11 10:52:54 +0900

    ok

---
+++ datascience/test_00011/인코딩.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_digits
+import numpy as np 
+import matplotlib.pyplot as plt
+
+digits = load_digits()
+
+plt.figure(figsize=(20,4))
+for index, (image, label) in enumerate(zip(digits.data[0:5], digits.target[0:5])):
+    plt.subplot(1, 5, index + 1)
+    plt.imshow(np.reshape(image, (8,8)), cmap=plt.cm.gray)
+    plt.title('Training: %i\n' % label, fontsize = 20)
+
+plt.show()
